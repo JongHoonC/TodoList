9ebee0149186a3b12dc39a218faf07067eaa901d
JongHoonC <bjhlove0126@naver.com>
2023-01-30 09:40:23 +0900

    sdfsdf

--- dist/js/app.c5243692.js
+++ dist/js/app.c5243692.js
@@ -1,2 +1,279 @@
-(function(){"use strict";var t={2963:function(t,o,e){var n=e(9242),r=e(3396);function i(t,o,e,n,i,l){const a=(0,r.up)("todolist");return(0,r.wg)(),(0,r.j4)(a)}var l=e(7139);const a=t=>((0,r.dD)("data-v-6a9a3874"),t=t(),(0,r.Cn)(),t),c=a((()=>(0,r._)("h3",null,"TodoList",-1))),u={class:"wrap"},d={class:"inputbox"},s=["onClick"],f=["onClick"],p=a((()=>(0,r._)("span",null,"-",-1))),m=[p];function v(t,o,e,i,a,p){return(0,r.wg)(),(0,r.iD)(r.HY,null,[c,(0,r._)("div",u,[(0,r._)("div",d,[(0,r.wy)((0,r._)("input",{type:"date","onUpdate:modelValue":o[0]||(o[0]=o=>t.todoDate=o),name:"",id:"date"},null,512),[[n.nr,t.todoDate]]),(0,r.wy)((0,r._)("input",{type:"text","onUpdate:modelValue":o[1]||(o[1]=o=>t.todoItem=o),id:"text",onKeyup:o[2]||(o[2]=(0,n.D2)(((...t)=>p.addTodo&&p.addTodo(...t)),["enter"]))},null,544),[[n.nr,t.todoItem]]),(0,r._)("button",{type:"submit",onClick:o[3]||(o[3]=(...t)=>p.addTodo&&p.addTodo(...t))},"+")]),(0,r._)("ul",null,[((0,r.wg)(!0),(0,r.iD)(r.HY,null,(0,r.Ko)(t.todolist,((t,o)=>((0,r.wg)(),(0,r.iD)("li",{key:t,class:(0,l.C_)({completed:t.complete}),onClick:e=>p.complete(t,o)},[(0,r.Uk)((0,l.zw)(t.dates)+"에는 "+(0,l.zw)(t.item)+" 을(를) 할 예정입니다. ",1),(0,r._)("button",{onClick:e=>p.removeTodo(t,o)},m,8,f)],10,s)))),128))])])],64)}e(7658);var h={data:function(){return{todoDate:"",todoItem:"",todolist:[]}},methods:{addTodo:function(){if(console.log(this.todoItem),null!=this.todoItem){const t={dates:this.todoDate,item:this.todoItem,complete:!1};localStorage.setItem(this.todoItem,JSON.stringify(t)),console.log(t),this.todolist.push(t),this.todoItem=""}},removeTodo:function(t,o){localStorage.removeItem(t.item),this.todolist.splice(o,1)},complete:function(t,o){t.complete=!t.complete,localStorage.removeItem(t.item),localStorage.setItem(t.complete,JSON.stringify(t))}},created:function(){if(localStorage.length>0)for(let t=0;t<localStorage.length;t++)"loglevel:webpack-dev-server"!==localStorage.key(t)&&this.todolist.push(JSON.parse(localStorage.getItem(localStorage.key(t))))}},g=e(89);const y=(0,g.Z)(h,[["render",v],["__scopeId","data-v-6a9a3874"]]);var b=y,w={name:"App",components:{todolist:b}};const _=(0,g.Z)(w,[["render",i],["__scopeId","data-v-55d577f7"]]);var k=_;(0,n.ri)(k).mount("#app")}},o={};function e(n){var r=o[n];if(void 0!==r)return r.exports;var i=o[n]={exports:{}};return t[n](i,i.exports,e),i.exports}e.m=t,function(){var t=[];e.O=function(o,n,r,i){if(!n){var l=1/0;for(d=0;d<t.length;d++){n=t[d][0],r=t[d][1],i=t[d][2];for(var a=!0,c=0;c<n.length;c++)(!1&i||l>=i)&&Object.keys(e.O).every((function(t){return e.O[t](n[c])}))?n.splice(c--,1):(a=!1,i<l&&(l=i));if(a){t.splice(d--,1);var u=r();void 0!==u&&(o=u)}}return o}i=i||0;for(var d=t.length;d>0&&t[d-1][2]>i;d--)t[d]=t[d-1];t[d]=[n,r,i]}}(),function(){e.n=function(t){var o=t&&t.__esModule?function(){return t["default"]}:function(){return t};return e.d(o,{a:o}),o}}(),function(){e.d=function(t,o){for(var n in o)e.o(o,n)&&!e.o(t,n)&&Object.defineProperty(t,n,{enumerable:!0,get:o[n]})}}(),function(){e.g=function(){if("object"===typeof globalThis)return globalThis;try{return this||new Function("return this")()}catch(t){if("object"===typeof window)return window}}()}(),function(){e.o=function(t,o){return Object.prototype.hasOwnProperty.call(t,o)}}(),function(){var t={143:0};e.O.j=function(o){return 0===t[o]};var o=function(o,n){var r,i,l=n[0],a=n[1],c=n[2],u=0;if(l.some((function(o){return 0!==t[o]}))){for(r in a)e.o(a,r)&&(e.m[r]=a[r]);if(c)var d=c(e)}for(o&&o(n);u<l.length;u++)i=l[u],e.o(t,i)&&t[i]&&t[i][0](),t[i]=0;return e.O(d)},n=self["webpackChunktodolist"]=self["webpackChunktodolist"]||[];n.forEach(o.bind(null,0)),n.push=o.bind(null,n.push.bind(n))}();var n=e.O(void 0,[998],(function(){return e(2963)}));n=e.O(n)})();
+(function () {
+  "use strict";
+  var t = {
+      2963: function (t, o, e) {
+        var n = e(9242),
+          r = e(3396);
+        function i(t, o, e, n, i, l) {
+          const a = (0, r.up)("todolist");
+          return (0, r.wg)(), (0, r.j4)(a);
+        }
+        var l = e(7139);
+        const a = (t) => (
+            (0, r.dD)("data-v-6a9a3874"), (t = t()), (0, r.Cn)(), t
+          ),
+          c = a(() => (0, r._)("h3", null, "TodoList", -1)),
+          u = { class: "wrap" },
+          d = { class: "inputbox" },
+          s = ["onClick"],
+          f = ["onClick"],
+          p = a(() => (0, r._)("span", null, "-", -1)),
+          m = [p];
+        function v(t, o, e, i, a, p) {
+          return (
+            (0, r.wg)(),
+            (0, r.iD)(
+              r.HY,
+              null,
+              [
+                c,
+                (0, r._)("div", u, [
+                  (0, r._)("div", d, [
+                    (0, r.wy)(
+                      (0, r._)(
+                        "input",
+                        {
+                          type: "date",
+                          "onUpdate:modelValue":
+                            o[0] || (o[0] = (o) => (t.todoDate = o)),
+                          name: "",
+                          id: "date",
+                        },
+                        null,
+                        512
+                      ),
+                      [[n.nr, t.todoDate]]
+                    ),
+                    (0, r.wy)(
+                      (0, r._)(
+                        "input",
+                        {
+                          type: "text",
+                          "onUpdate:modelValue":
+                            o[1] || (o[1] = (o) => (t.todoItem = o)),
+                          id: "text",
+                          onKeyup:
+                            o[2] ||
+                            (o[2] = (0, n.D2)(
+                              (...t) => p.addTodo && p.addTodo(...t),
+                              ["enter"]
+                            )),
+                        },
+                        null,
+                        544
+                      ),
+                      [[n.nr, t.todoItem]]
+                    ),
+                    (0, r._)(
+                      "button",
+                      {
+                        type: "submit",
+                        onClick:
+                          o[3] ||
+                          (o[3] = (...t) => p.addTodo && p.addTodo(...t)),
+                      },
+                      "+"
+                    ),
+                  ]),
+                  (0, r._)("ul", null, [
+                    ((0, r.wg)(!0),
+                    (0, r.iD)(
+                      r.HY,
+                      null,
+                      (0, r.Ko)(
+                        t.todolist,
+                        (t, o) => (
+                          (0, r.wg)(),
+                          (0, r.iD)(
+                            "li",
+                            {
+                              key: t,
+                              class: (0, l.C_)({ completed: t.complete }),
+                              onClick: (e) => p.complete(t, o),
+                            },
+                            [
+                              (0, r.Uk)(
+                                (0, l.zw)(t.dates) +
+                                  "에는 " +
+                                  (0, l.zw)(t.item) +
+                                  " 을(를) 할 예정입니다. ",
+                                1
+                              ),
+                              (0, r._)(
+                                "button",
+                                { onClick: (e) => p.removeTodo(t, o) },
+                                m,
+                                8,
+                                f
+                              ),
+                            ],
+                            10,
+                            s
+                          )
+                        )
+                      ),
+                      128
+                    )),
+                  ]),
+                ]),
+              ],
+              64
+            )
+          );
+        }
+        e(7658);
+        var h = {
+            data: function () {
+              return { todoDate: "", todoItem: "", todolist: [] };
+            },
+            methods: {
+              addTodo: function () {
+                if ((console.log(this.todoItem), null != this.todoItem)) {
+                  const t = {
+                    dates: this.todoDate,
+                    item: this.todoItem,
+                    complete: !1,
+                  };
+                  localStorage.setItem(this.todoItem, JSON.stringify(t)),
+                    console.log(t),
+                    this.todolist.push(t),
+                    (this.todoItem = "");
+                }
+              },
+              removeTodo: function (t, o) {
+                localStorage.removeItem(t.item), this.todolist.splice(o, 1);
+              },
+              complete: function (t, o) {
+                (t.complete = !t.complete),
+                  localStorage.removeItem(t.item),
+                  localStorage.setItem(t.complete, JSON.stringify(t));
+              },
+            },
+            created: function () {
+              if (localStorage.length > 0)
+                for (let t = 0; t < localStorage.length; t++)
+                  "loglevel:webpack-dev-server" !== localStorage.key(t) &&
+                    this.todolist.push(
+                      JSON.parse(localStorage.getItem(localStorage.key(t)))
+                    );
+            },
+          },
+          g = e(89);
+        const y = (0, g.Z)(h, [
+          ["render", v],
+          ["__scopeId", "data-v-6a9a3874"],
+        ]);
+        var b = y,
+          w = { name: "App", components: { todolist: b } };
+        const _ = (0, g.Z)(w, [
+          ["render", i],
+          ["__scopeId", "data-v-55d577f7"],
+        ]);
+        var k = _;
+        (0, n.ri)(k).mount("#app");
+      },
+    },
+    o = {};
+  function e(n) {
+    var r = o[n];
+    if (void 0 !== r) return r.exports;
+    var i = (o[n] = { exports: {} });
+    return t[n](i, i.exports, e), i.exports;
+  }
+  (e.m = t),
+    (function () {
+      var t = [];
+      e.O = function (o, n, r, i) {
+        if (!n) {
+          var l = 1 / 0;
+          for (d = 0; d < t.length; d++) {
+            (n = t[d][0]), (r = t[d][1]), (i = t[d][2]);
+            for (var a = !0, c = 0; c < n.length; c++)
+              (!1 & i || l >= i) &&
+              Object.keys(e.O).every(function (t) {
+                return e.O[t](n[c]);
+              })
+                ? n.splice(c--, 1)
+                : ((a = !1), i < l && (l = i));
+            if (a) {
+              t.splice(d--, 1);
+              var u = r();
+              void 0 !== u && (o = u);
+            }
+          }
+          return o;
+        }
+        i = i || 0;
+        for (var d = t.length; d > 0 && t[d - 1][2] > i; d--) t[d] = t[d - 1];
+        t[d] = [n, r, i];
+      };
+    })(),
+    (function () {
+      e.n = function (t) {
+        var o =
+          t && t.__esModule
+            ? function () {
+                return t["default"];
+              }
+            : function () {
+                return t;
+              };
+        return e.d(o, { a: o }), o;
+      };
+    })(),
+    (function () {
+      e.d = function (t, o) {
+        for (var n in o)
+          e.o(o, n) &&
+            !e.o(t, n) &&
+            Object.defineProperty(t, n, { enumerable: !0, get: o[n] });
+      };
+    })(),
+    (function () {
+      e.g = (function () {
+        if ("object" === typeof globalThis) return globalThis;
+        try {
+          return this || new Function("return this")();
+        } catch (t) {
+          if ("object" === typeof window) return window;
+        }
+      })();
+    })(),
+    (function () {
+      e.o = function (t, o) {
+        return Object.prototype.hasOwnProperty.call(t, o);
+      };
+    })(),
+    (function () {
+      var t = { 143: 0 };
+      e.O.j = function (o) {
+        return 0 === t[o];
+      };
+      var o = function (o, n) {
+          var r,
+            i,
+            l = n[0],
+            a = n[1],
+            c = n[2],
+            u = 0;
+          if (
+            l.some(function (o) {
+              return 0 !== t[o];
+            })
+          ) {
+            for (r in a) e.o(a, r) && (e.m[r] = a[r]);
+            if (c) var d = c(e);
+          }
+          for (o && o(n); u < l.length; u++)
+            (i = l[u]), e.o(t, i) && t[i] && t[i][0](), (t[i] = 0);
+          return e.O(d);
+        },
+        n = (self["webpackChunktodolist"] = self["webpackChunktodolist"] || []);
+      n.forEach(o.bind(null, 0)), (n.push = o.bind(null, n.push.bind(n)));
+    })();
+  var n = e.O(void 0, [998], function () {
+    return e(2963);
+  });
+  n = e.O(n);
+})();
 //# sourceMappingURL=app.c5243692.js.map
